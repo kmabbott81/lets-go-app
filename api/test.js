9e614b7382dc14f3e5af4995147bd7a35af15ff3
Kyle Mabbott <kbmabb@gmail.com>
2025-09-27 14:52:29 -0700

    Add API test endpoint to debug Vercel functions

---
+++ api/test.js
@@ -0,0 +1,16 @@
+module.exports = async function handler(req, res) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+    if (req.method === 'OPTIONS') {
+        return res.status(200).end();
+    }
+
+    res.json({
+        success: true,
+        message: "API is working!",
+        hasGoogleKey: !!process.env.GOOGLE_PLACES_API_KEY,
+        hasGeminiKey: !!process.env.GEMINI_API_KEY
+    });
+};
